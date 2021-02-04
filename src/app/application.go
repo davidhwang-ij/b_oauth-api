88f6841ab57e5b3d2c1ea86a8114e12c4d4ce619
David Hwang <davidhwang.ij@gmail.com>
2021-02-03 21:54:26 -0500

    oauth create & getUserId

--- src/app/application.go
+++ src/app/application.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"github.com/davidhwang-ij/bookstore_oauth-api/src/clients/cassandra"
 	"github.com/davidhwang-ij/bookstore_oauth-api/src/domain/access_token"
 	"github.com/davidhwang-ij/bookstore_oauth-api/src/http"
 	"github.com/davidhwang-ij/bookstore_oauth-api/src/repository/db"
@@ -12,9 +13,16 @@ var (
 )
 
 func StartApplication() {
+	session, dbErr := cassandra.GetSession()
+	if dbErr != nil {
+		panic(dbErr)
+	}
+	session.Close()
+
 	atHandler := http.NewHandler(access_token.NewService(db.NewRepository()))
 
 	router.GET("/oauth/access_token/:access_token_id", atHandler.GetById)
+	router.POST("/oauth/access_token", atHandler.Create)
 
 	router.Run(":8080")
 }
--- src/http/http_access_token.go
+++ src/http/http_access_token.go
@@ -4,11 +4,13 @@ import (
 	"net/http"
 
 	"github.com/davidhwang-ij/bookstore_oauth-api/src/domain/access_token"
+	"github.com/davidhwang-ij/bookstore_oauth-api/src/utils/errors"
 	"github.com/gin-gonic/gin"
 )
 
 type AccessTokenHandler interface {
-	GetById(c *gin.Context)
+	GetById(*gin.Context)
+	Create(*gin.Context)
 }
 
 type accessTokenHandler struct {
@@ -28,4 +30,20 @@ func (handler *accessTokenHandler) GetById(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, accessToken)
+}
+
+func (handler *accessTokenHandler) Create(c *gin.Context) {
+	var at access_token.AccessToken
+	if err := c.ShouldBindJSON(at); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	if err := handler.service.Create(at); err != nil {
+		c.JSON(err.Status, err)
+		return
+	}
+
+	c.JSON(http.StatusCreated, at)
 }
