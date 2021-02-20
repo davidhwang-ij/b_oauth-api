622a861a5391e88095b868ee4d52d8c8b9d3133a
David Hwang <davidhwang.ij@gmail.com>
2021-02-19 22:33:39 -0500

    Added LoginUser

--- src/repository/rest/users_repository.go
+++ src/repository/rest/users_repository.go
@@ -1,8 +1,19 @@
 package rest
 
 import (
+	"encoding/json"
+	"time"
+
+	"github.com/davidhwang-ij/bookstore_oauth-api/golang-restclient/rest"
 	"github.com/davidhwang-ij/bookstore_oauth-api/src/domain/users"
 	"github.com/davidhwang-ij/bookstore_oauth-api/src/utils/errors"
+)
+
+var (
+	usersRestClient = rest.RequestBuilder{
+		BaseURL: "https://api.bookstore.com",
+		Timeout: 100 * time.Millisecond,
+	}
 )
 
 type RestUsersRepository interface {
@@ -16,5 +27,26 @@ func NewRepository() RestUsersRepository {
 }
 
 func (r *usersRepository) LoginUser(email string, password string) (*users.User, *errors.RestErr) {
-	return nil, nil
+	request := users.UserLoginRequest{
+		Email:    email,
+		Password: password,
+	}
+	response := usersRestClient.Post("/users/login", request)
+	if response == nil || response.Response == nil {
+		return nil, errors.NewInternalServerError("invalid restclient response when trying to login user")
+	}
+	if response.StatusCode > 299 {
+		var restErr errors.RestErr
+		err := json.Unmarshal(response.Bytes(), &restErr)
+		if err != nil {
+			return nil, errors.NewInternalServerError("invalid error interface when trying to login user")
+		}
+		return nil, &restErr
+	}
+
+	var user users.User
+	if err := json.Unmarshal(response.Bytes(), &user); err != nil {
+		return nil, errors.NewInternalServerError("error when trying to unmarshal users response")
+	}
+	return &user, nil
 }
